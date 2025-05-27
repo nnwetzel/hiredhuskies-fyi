98741f83ad97c53a889690fc29a4558abfbe8d7f
Nat Wetzel <wetzel.na@northeastern.edu>
2025-05-27 16:01:11 -0700

    Fix search

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -1,11 +1,9 @@
-'use client';
-
 import { Suspense } from 'react';
 import ReviewsPageContent from './ReviewsPageContent';
 
 export default function ReviewsPage() {
   return (
-    <Suspense fallback={<div className="text-center mt-10">Loading reviews...</div>}>
+    <Suspense fallback={<div className="text-center mt-10">Loading...</div>}>
       <ReviewsPageContent />
     </Suspense>
   );
